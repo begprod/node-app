c0078618d9c7b974688180ea36ce5b44aba635d2
Andrey Aratov <beg431@yandex.ru>
2020-08-31 15:33:00 +0300

    added users page

---
+++ client/src/views/Users.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="users">
+    <h1>Users</h1>
+    <div v-for="user in users" v-bind:key="user.id" class="user">
+      <div class="user__name">
+        {{ user["userFirstName"] }}
+      </div>
+      <div class="user__second-name">
+        {{ user["userFirstName"] }}
+      </div>
+      <div class="user__phone">
+        {{ user["userFirstName"] }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+      users: null
+    }
+  },
+  methods: {
+    getUsers() {
+      this.$axios.get('http://localhost:3000/api/users')
+      .then((response) => {
+        this.users = response.data;
+      })
+    }
+  },
+  mounted() {
+    this.getUsers();
+  }
+}
+</script>
+
+<style lang="scss">
+.user {
+  border: 1px solid red;
+
+  &__name {
+    font-size: 36px;
+  }
+
+  &__second-name {
+
+  }
+
+  &__phone {
+
+  }
+}
+</style>
